deac0f82b4b536dfdb0e8f54daa0706cee8c8b83
lucasrouco <lucasgonzalezrouco@gmail.com>
2020-07-10 00:04:13 -0300

    links distintos segun tipo de usuario

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -9,9 +12,19 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class ToolbarComponent implements OnInit {
 
+  public user$: Observable<any> = this.authSvc.afAuth.user;
+  public userData;
+  public loggedUser: any = '';
+  public esEmpresa: boolean = null
+
+  ngOnInit(): void {
+    this.userData = this.verificarSiAlguienInicio();
+  }
+
   constructor(
     private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
+    private domSanitizer: DomSanitizer,
+    private authSvc: AuthService, private router: Router
   ) {
     this.matIconRegistry.addSvgIcon(
       `home`,
@@ -23,7 +36,30 @@ export class ToolbarComponent implements OnInit {
     );
   }
 
-  ngOnInit(): void {
+  verificarSiAlguienInicio() {
+    try {
+      this.authSvc.afAuth.user.subscribe((u) => {
+        if (u) {
+
+          this.authSvc.getUser(u.uid).subscribe((userSnapshot) => {
+
+            this.loggedUser = userSnapshot.payload.data();
+            console.log(userSnapshot.payload.exists);
+            if(this.loggedUser.empresa){
+              this.esEmpresa = true;
+            } else {
+              this.esEmpresa = false;
+            }
+          })
+          return u;
+        } else {
+          console.log("Nadie inicio sesion");
+        }
+
+      })
+    } catch (error) {
+      console.log(error);
+    }
   }
 
 }
