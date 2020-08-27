9be4e8e701aa46da4c0dbf911c8d083744b3e9bf
valentina gottelli <valugottelli@gmail.com>
2020-08-27 00:12:51 -0300

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB7qv8Xf5ZbfZSqJ6yvkoSUZjJEa51nyvY",
+  authDomain: "red-egresados-online.firebaseapp.com",
+  databaseURL: "https://red-egresados-online.firebaseio.com",
+  projectId: "red-egresados-online",
+  storageBucket: "red-egresados-online.appspot.com",
+  messagingSenderId: "886046332432",
+  appId: "1:886046332432:web:7d931a7ffd26c29ba81c95",
+  measurementId: "G-KF7NXTSYPF"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,15 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBCJdErLttb6rqGgxYUCuM78MhozVLX8U0",
-    authDomain: "red-de-egresados.firebaseapp.com",
-    databaseURL: "https://red-de-egresados.firebaseio.com",
-    projectId: "red-de-egresados",
-    storageBucket: "red-de-egresados.appspot.com",
-    messagingSenderId: "721095525508",
-    appId: "1:721095525508:web:a6972b3a6c4a4daa5f6ed1",
-    measurementId: "G-VFQB7BY87G"
-  },
+    apiKey: "AIzaSyB7qv8Xf5ZbfZSqJ6yvkoSUZjJEa51nyvY",
+  authDomain: "red-egresados-online.firebaseapp.com",
+  databaseURL: "https://red-egresados-online.firebaseio.com",
+  projectId: "red-egresados-online",
+  storageBucket: "red-egresados-online.appspot.com",
+  messagingSenderId: "886046332432",
+  appId: "1:886046332432:web:7d931a7ffd26c29ba81c95",
+  measurementId: "G-KF7NXTSYPF"
+  }
 };
 
 /*
