036ba04e1b1aba9be71e8516755db6b38ad20dd7
lucasrouco <lucasgonzalezrouco@gmail.com>
2020-09-10 14:42:10 -0300

    fix routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -108,7 +108,7 @@ import { LocationStrategy, HashLocationStrategy } from '@angular/common';
   ],
   providers: [
     {provide: MAT_DATE_LOCALE, useValue: 'en-GB'},
-    /* {provide: LocationStrategy, useClass: HashLocationStrategy} */
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
   ],
   bootstrap: [AppComponent],
 })
